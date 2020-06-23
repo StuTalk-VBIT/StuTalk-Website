53dae553846034b26577c629aece1757c9a3a129
yasaswiraj <47531308+yasaswiraj@users.noreply.github.com>
2020-06-23 14:07:26 +0530

    animation

---
+++ Assets/animations/intro.js
@@ -0,0 +1,7 @@
+var animItem = bodymovin.loadAnimation({
+  container: document.getElementById('anim'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: './Assets/animations/data.json'
+});
